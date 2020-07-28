c595a79a2019392ec0008d4c9debc1ea22092247
Leana Critchell <leana.m.critchell@gmail.com>
2020-07-27 18:22:02 -0700

    lots of eda of all dfs.  created new dfs of cleaned and joined dfs in data folder

---
+++ src/visualisations.py
@@ -0,0 +1,18 @@
+"""
+Plot parameters to use for visualisations to ensure standardisation
+between plots.
+
+For now copy and paste (and update where you like) to add into your notebook.
+
+Will eventually update this so that it's a function of sorts that can 
+be read in and run in a notebook. 
+
+Run `plt.rcParams` to see all available options that can be changed. 
+"""
+
+plt.rcParams['axes.labelsize'] = 20
+plt.rcParams['axes.titlesize'] = 25
+plt.rcParams['xtick.labelsize'] = 15
+plt.rcParams['ytick.labelsize'] = 15
+plt.rcParams['axes.edgecolor'] = 'black'
+plt.rcParams['axes.facecolor'] = 'white' # or EAEAF2
